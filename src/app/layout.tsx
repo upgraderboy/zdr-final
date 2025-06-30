a07c361c5aef0c15ecf408fd10e9a8e69d870860
Upgrader Boy <upgraderboy@gmail.com>
2025-06-30 18:47:01 +0530

    code published

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,16 @@ import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import { TRPCProvider } from '@/trpc/client'
 import { Toaster } from "@/components/ui/sonner";
-const inter = Inter({ subsets: ["latin"] })
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ZDAR",
   description: "Job Portal",
 };
-// Add ThemeProvider
-import { ThemeProvider } from "next-themes";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,19 +22,70 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body
-          className={inter.className}
-        >
+        <head>
+          {/* Google Analytics - gtag */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-M02BD7F8PJ"
+            strategy="afterInteractive"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-M02BD7F8PJ');
+              `,
+            }}
+          />
+
+          {/* Google Tag Manager */}
+          <Script
+            id="gtm-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NJQFWKCQ');
+              `,
+            }}
+          />
+
+          {/* Cookie Consent */}
+          <Script
+            id="cookie-consent"
+            src="https://policy.app.cookieinformation.com/uc.js"
+            data-culture="EN"
+            data-gcm-version="2.0"
+            strategy="afterInteractive"
+          />
+        </head>
+        <body className={inter.className}>
+          {/* GTM noscript fallback */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NJQFWKCQ"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-          <TRPCProvider>
-            {children}
-            <Toaster />
-          </TRPCProvider>
+            <TRPCProvider>
+              {children}
+              <Toaster />
+            </TRPCProvider>
           </ThemeProvider>
         </body>
       </html>
